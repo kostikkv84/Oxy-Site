97f803429e1e25bd2da3d5d5ea042100ded302e9
koskv <koskv@list.ru>
2019-09-02 14:37:36 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$('.js-click-modal').click(function(){
+  $('.button').addClass('modal-open');
+});
+
+$('.js-close-modal').click(function(){
+  $('.button').removeClass('modal-open');
+});
+
+/* ----------menu ------------------ */
+
